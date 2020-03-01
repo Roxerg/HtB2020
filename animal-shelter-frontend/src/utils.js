7288afc3d2f911cb119e7e0a2221439baf55c4d5
Findlay Smith <findlayksmith@gmail.com>
2020-03-01 12:28:36 +0000

    minor commit

--- animal-shelter-frontend/src/utils.js
+++ animal-shelter-frontend/src/utils.js
@@ -97,6 +97,16 @@ export const addPost = async options => {
 };
 
 export const getPosts = async (setPosts, setDirty) => {
+    const r = await fetch(`${apiUrl}/posts/user/current`, {
+        method: "GET",
+        credentials: "include"
+    });
+    const json = await r.json();
+    setPosts(json);
+    setDirty(false);
+};
+
+export const getCurrentUserPosts = async (setPosts, setDirty) => {
     const r = await fetch(`${apiUrl}/posts/get`, {
         method: "GET",
         credentials: "include"
